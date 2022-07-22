0b6bbe1f2408e81359ff9849c290721a6397a8a6
211218 <211218@ids.upchiapas.edu.mx>
2022-07-22 12:00:22 -0700

    new commir

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("hello")
+
 public class UserController {
-    @RequestMapping("/")
+
+    @GetMapping("person")
     public String home() {
         return "Hello Luis and Gerardo ------- teacher : colochin";
     }
-
 }
