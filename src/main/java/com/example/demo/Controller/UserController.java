8b5a722e95e8c5c0fc72c150ef3487ebd8c76613
Hum-berto <211134@ids.upchiapas.edu.mx>
2022-07-25 10:52:56 -0500

    add all

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -12,6 +12,6 @@ public class UserController {
 
     @GetMapping("person")
     public String home() {
-        return "Hello Luis and Gerardo ------- teacher : colochin";
+        return "Hello Luis and Gerardo -------> teacher : colochin";
     }
 }
